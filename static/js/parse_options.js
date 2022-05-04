a1f8705f86c173c65a33f95f6bc770bc5b841e5b
KrisMarboe <s194249@student.dtu.dk>
2022-05-04 13:15:00 +0200

    clean up

--- static/js/parse_options.js
+++ static/js/parse_options.js
@@ -1,13 +1,11 @@
 // =========== GET DATA AND CONFIG FROM URL PARAMETERS =================
 window.addEventListener('load', () => {
-    network_load('../networks/full/')
+    network_load('../networks/rap/withsingles/genre/')
 })
+
 function network_load(d) {
     controls = {};
-    console.log(window.location.href + d)
     controls['file_path'] = d + 'network.json';
-
-
 
     d3.json(d + 'config.json', new_controls => {
         controls = {...controls, ...new_controls}
